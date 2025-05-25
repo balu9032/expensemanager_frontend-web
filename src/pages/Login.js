c599f75a5c0df0c74d539410da86a837636e6e79
Bala Bharath Doppalapudi <balabharath.d@gmail.com>
2025-05-26 01:17:39 +0530

    show password is commented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
+    // const [showPassword, setShowPassword] = useState(false);
     const navigate = useNavigate();
 
     const handleLogin = (e) => {
